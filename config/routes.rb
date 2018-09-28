770fca30e561f36961e66957d372eee85e40ac47
YousukeOchi <yowyowyou6@gmail.com>
2018-09-28 12:09:34 +0900

    Css適用のため微修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get 'hosts' => 'hosts#index1'
   get 'hosts/new' => 'hosts#new'
   post 'hosts' => 'hosts/create'
-
+  get 'tests/show' => 'tests#show' #とりあえず仮置き
 end
