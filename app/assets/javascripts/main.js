1c1dc75c54ade58eb66f93986c48c294c566590d
yodako <yoda@sana.local>
2018-09-28 16:30:03 +0900

    Rooms リスト表示

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -9,13 +9,5 @@ $(function() {
           $(this).text("条件、プライバシー、通貨など");
           flg = "default";
         }
-        // if ($('#footer-link_text1').text('条件、プライバシー、通貨など')) {
-        //     var str = $('#footer-link_text1').text().replace(/条件、プライバシー、通貨など/g, '閉じる');
-        //     $('#footer-link_text1').text(str);
-        // }
-        // if ($('#footer-link_text1').text('閉じる')) {
-        //     var str = $('#footer-link_text1').text().replace(/条閉じる/g, '条件、プライバシー、通貨など');
-        //     $('#footer-link_text1').text(str);
-        // }
     });
 });
