e26738de085f1b458f85dbb864fbbf3d4246c85d
yodako <yoda@sana.local>
2018-09-28 09:35:26 +0900

    footer　表示非表示

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,21 @@
 $(function() {
-
-  console.log($("#title"));
-
+    var flg = "default";
+    $('#footer-button').click(function(){
+        $('.footer').toggle();
+        if(flg == "default"){
+          $(this).text("閉じる");
+          flg = "changed";
+        }else{
+          $(this).text("条件、プライバシー、通貨など");
+          flg = "default";
+        }
+        // if ($('#footer-link_text1').text('条件、プライバシー、通貨など')) {
+        //     var str = $('#footer-link_text1').text().replace(/条件、プライバシー、通貨など/g, '閉じる');
+        //     $('#footer-link_text1').text(str);
+        // }
+        // if ($('#footer-link_text1').text('閉じる')) {
+        //     var str = $('#footer-link_text1').text().replace(/条閉じる/g, '条件、プライバシー、通貨など');
+        //     $('#footer-link_text1').text(str);
+        // }
+    });
 });
