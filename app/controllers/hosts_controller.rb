72c29b8aa018dcc1bec46abc9f6b8b2f22b80b2d
takahirohayasaka <hayasakatakahiro@hayasakatadahironoMacBook-puro.local>
2018-09-28 10:37:30 +0900

    仮置きのコントローラ設定

---
+++ app/controllers/hosts_controller.rb
@@ -0,0 +1,18 @@
+class HostsController < ApplicationController
+  def index
+  end
+  
+  def new
+    @host = Host.new
+  end
+
+  def create
+    Host.create(room_params)
+    redirect_to action:index
+  end
+
+  private
+  def room_params
+    params.require(:host).permit(:kind,:note,:no,:ki)
+  end
+end
